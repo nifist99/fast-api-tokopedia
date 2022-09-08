63452e930c5bf05fa95cfe8c79a6eb66e69d1524
aldrichzefanya3 <aldrichzefanya3@gmail.com>
2022-09-09 01:17:01 +0700

    jwt

--- app/helper/Utils.py
+++ app/helper/Utils.py
@@ -2,15 +2,43 @@ from passlib.context import CryptContext
 import os
 from datetime import datetime, timedelta
 from typing import Union, Any
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from dotenv import dotenv_values,load_dotenv
+from fastapi import HTTPException,Depends,status
+from pydantic import BaseModel
 
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv['ACCESS_TOKEN_EXPIRE_MINUTES']  # 30 minutes
-REFRESH_TOKEN_EXPIRE_MINUTES = os.getenv['REFRESH_TOKEN_EXPIRE_MINUTES'] # 7 days
-ALGORITHM = os.getenv['ALGORITHM']
-JWT_SECRET_KEY = os.getenv['SECRET_KEY']     # should be kept secret
-JWT_REFRESH_SECRET_KEY = os.getenv['SECRET_KEY']      # should be kept secret
+config = dotenv_values(".env")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30 # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30 # 7 days
+ALGORITHM = config['ALGORITHM']
+JWT_SECRET_KEY = config['SECRET_KEY']     # should be kept secret
+JWT_REFRESH_SECRET_KEY = config['SECRET_KEY']      # should be kept secret
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_hashed_password(password: str) -> str:
     return password_context.hash(password)
@@ -39,3 +67,22 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = 'niko'
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Union
+from datetime import datetime, timedelta
 import uvicorn
 from fastapi import FastAPI
+
+from app.helper.Utils import create_access_token,create_refresh_token,verify_password,get_hashed_password,ACCESS_TOKEN_EXPIRE_MINUTES
 # from routes.route import auth
 
 app = FastAPI()
@@ -8,5 +11,11 @@ app = FastAPI()
 # app.mount('/api/v1',auth)
 
 if __name__ == '__main__':
+    print(get_hashed_password('123'))
+    print(verify_password('123','$2b$12$v6rqq955NiX6g3dUYq7gNuQlHxfZSyrv/Xk0UigLV82R/Sgx0JoEi'))
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        'niko', expires_delta=access_token_expires
+    )
+    print(access_token)
     uvicorn.run("main:app", host='localhost', port=8000, log_level="info", reload=True)
-    print("running")
