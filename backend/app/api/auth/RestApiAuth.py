bcd6726759d013160aceabec76c24091dd66094d
aldrichzefanya3 <aldrichzefanya3@gmail.com>
2022-09-09 16:36:57 +0700

    auth process

--- backend/app/api/auth/RestApiAuth.py
+++ backend/app/api/auth/RestApiAuth.py
@@ -10,7 +10,9 @@ from uuid import uuid4
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-from backend.app.helper.Utils import get_hashed_password
+from backend.app.helper.Utils import get_hashed_password,create_access_token,ACCESS_TOKEN_EXPIRE_MINUTES
+from backend.app.validation.UsersValidate import UsersValidate
+from datetime import datetime, timedelta
 
 
 restAuth = APIRouter(prefix="/auth")
@@ -27,9 +29,11 @@ class body_auth_register(BaseModel):
 
 @restAuth.post("/login")
 async def login(email: str = Form(), password: str = Form()):
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
                 "email":email,
-                "password":password
+                "password":get_hashed_password(password),
+                "token" : create_access_token(email,expires_delta=access_token_expires)
             }
 
 @restAuth.post("/register")
@@ -56,12 +60,24 @@ async def forget(request : Request,respone : Response):
 @restAuth.post("/logout")
 async def logout(request : Request,respone : Response):
     req = await request.json()
-    respone.status_code = status.HTTP_400_BAD_REQUEST
-    return {
-        "status":True,
-        "code":respone.status_code,
-        "message":"success logout",
-        "data":req
-    }
+    print(req)
+    # validasi
+    validate = UsersValidate.forgotPassword(req)
+    print(validate)
+    if(validate['status']):
+        respone.status_code = status.HTTP_200_OK
+        return {
+            "status":True,
+            "code":respone.status_code,
+            "message":validate['message'],
+            "data":req
+        }
+    else:
+        respone.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "status":False,
+            "code":respone.status_code,
+            "message":validate['message'],
+        }
          
             
